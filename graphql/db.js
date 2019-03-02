f25334ada25789d99b5cd57467abf9552c028a0a
duddn14758 <duddn14758@naver.com>
2019-03-02 16:34:32 +0900

    graphql query

--- graphql/db.js
+++ graphql/db.js
@@ -32,6 +32,52 @@ export const people = [
 ];
 
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === String(id));
-    return filteredPeople[0];
+    const filteredMovies = movies.filter(movie => movie.id === id);
+    return filteredMovies[0];
+}
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if(movies.length > cleanedMovies.length){
+        movies = cleanedMovies;
+        return true;
+    }else{
+        return false;
+    }
+};
+
+let movies = [
+    {
+        id: 1,
+        name: "god father",
+        score: 10
+    },
+    {
+        id: 2,
+        name: "god mother",
+        score: 20
+    },
+    {
+        id: 3,
+        name: "god grandfather",
+        score: 30
+    },
+    {
+        id: 4,
+        name: "got grandmother",
+        score: 40
+    }
+];
+
+export const getMovies = () => movies;
+
+export const addMovie = (name,score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    console.log("add "+newMovie.name);
+    return newMovie;
 }
