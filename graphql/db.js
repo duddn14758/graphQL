c75949284eb2788c0836ce600201d71678c26e3f
duddn14758 <duddn14758@naver.com>
2019-03-02 20:41:46 +0900

    add function

--- graphql/db.js
+++ graphql/db.js
@@ -1,35 +1,86 @@
-export const people = [
+let info_list = [
     {
-        id:"0",
-        name:"Young",
-        age:26,
-        gender:"male"
+        title:"title1",
+        contents:"Next, set up a form for capturing details about a GitHub organization and repository from a user. It should be possible to fill out an input field to request a paginated list of issues for a specific GitHub repository. ",
+        classify:"origin"
     },
+   {
+    title:"title2",
+    contents:"hello my name is",
+    classify:"origin"
+   },
+   {
+    title:"부경대학교",
+    contents:"부산 국립 부경대학교",
+    classify:"학교"
+   }
+];
+
+let reference = [
     {
-        id:"1",
-        name:"Woo",
-        age:16,
-        gender:"female"
-    },
-    {
-        id:"2",
-        name:"Gloria",
-        age:18,
-        gender:"male"
-    },
-    {
-        id:"3",
-        name:"Yeyoung",
-        age:24,
-        gender:"female"
-    },
-    {
-        id:"4",
-        name:"You",
-        age:6,
-        gender:"male"
+        reference:1,
+        parents:"title1",
+        child:"title2"
     }
 ];
+
+let history=[
+    {
+        edit_code:123,
+        title:"title1",
+        id:"young",
+        edit_contents:"hellllooolldf",
+        date:"2019-03-02"
+    },
+    {
+        edit_code:124,
+        title:"title1",
+        id:"young",
+        edit_contents:"wwoowowoowwowowwwowowo",
+        date:"2019-03-02"
+    },
+    {
+        edit_code:125,
+        title:"부경대학교",
+        id:"young",
+        edit_contents:"홀롤롤ㄹ롤ㄹㄹ",
+        date:"2019-03-01"
+    },
+    {
+        edit_code:126,
+        title:"부경대학교",
+        id:"dokdokorea",
+        edit_contents:"구국국",
+        date:"2019-03-02"
+    }
+];
+
+let user_info=[
+    {
+        id:"young",
+        nick_name:"duddn14758",
+        password:"1234",
+        email:"duddn14758@naver.com",
+        is_signed:true,
+        token:true
+    }
+];
+
+export const getContentsByTitle = title => {
+    const searchedContents=info_list.filter(content => content.title === title);
+    console.log(title);
+    return searchedContents[0];
+}
+
+export const getHistoriesByTitle = title => {
+    const filteredHistories = history.filter(histories => histories.title === title);
+    return filteredHistories;
+}
+
+export const getHistoriesById = id => {
+    const filteredHistories = history.filter(histories => histories.id === id);
+    return filteredHistories;
+}
 
 export const getById = id => {
     const filteredMovies = movies.filter(movie => movie.id === id);
@@ -80,4 +131,16 @@ export const addMovie = (name,score) => {
     movies.push(newMovie);
     console.log("add "+newMovie.name);
     return newMovie;
+}
+
+export const addContent = (title,contents,classify) => {
+    const newContent = {
+        title,
+        contents,
+        classify
+    };
+    console.log(title);
+    info_list.push(newContent);
+    console.log("add "+newContent.title);
+    return newContent;
 }
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,20 @@
-import {getMovies, getById, addMovie, deleteMovie} from "./db";
+import {getMovies, getById, addMovie, deleteMovie, getContentsByTitle,getHistoriesByTitle,
+     getRecentEditTimeByTitle,
+     getHistoriesById, addContent} from "./db";
+import { getIntrospectionQuery } from "graphql";
 
 const resolvers = {
     Query: {
         movies : () => getMovies(),
-        movie : (_, {id}) => getById(id)
+        movie : (_, {id}) => getById(id),
+        content_by_title : (_,{title}) => getContentsByTitle(title),     // 해당 글 조회시 나오는 내용
+        histories_by_title :(_,{title}) => getHistoriesByTitle(title),    // 해당 글 수정 히스토리
+        histories_by_id :(_,{id}) => getHistoriesById(id)
     },
     Mutation: {
         addMovie: (_,{name,score}) => addMovie(name,score),
-        deleteMovie: (_,{id}) => deleteMovie(id)
+        deleteMovie: (_,{id}) => deleteMovie(id),
+        addContent: (_,{title,contents,classify}) => addContent(title,contents,classify)
     }
 }
 
